c0923922890983ea443c6918032fea5740ff5cee
substantialcattle5 <nilaynathsharan16@gmail.com>
2025-02-26 21:42:13 +0530

    :sparkles: added the admin endpoint

---
+++ prisma/migrations/20250224093954_adding_lead_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Lead" ADD COLUMN     "status" "LeadStatus" NOT NULL DEFAULT 'NEW';
---
+++ prisma/migrations/20250224094200_adding_lead_prioirty/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Lead" ADD COLUMN     "priority" "Priority" NOT NULL DEFAULT 'LOW';
